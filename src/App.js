fba35f94751bb08bb053bb9a9826420ff59bcbb4
slymnsarii <slymnsarii@gmail.com>
2023-01-16 21:55:46 +0300

    day04

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Dersler from "./components/component1/dersler";
 import Paragraf from "./components/component10/paragraf";
 import Listele from "./components/component11/listele";
+import Countries from "./components/component12/countries";
+import Tours from "./components/component13/tours";
 import Practice2 from "./components/component2/practice2";
 import Practice3 from "./components/component3/practice3";
 import Tablo from "./components/component4/tablo";
@@ -15,17 +17,17 @@ function App() {
     <>
       {/* <Dersler/> 
     <Practice2/>
-    <Practice3 />
-    <Tablo />
-    <Ulkeler />
-    <Kisiler />
-    <Cards />
-    <RandomImage />
-    <Bilgiler />
-    <Paragraf />
-    */}
-
-      <Listele />
+    <Practice3/>
+      <Tablo/>
+      <Ulkeler/>
+      <Kisiler/>*/}
+      {/* <Cards/>
+      <RandomImage/>
+      <Bilgiler/>
+      <Paragraf/> 
+      <Listele/>
+         <Countries/>*/}
+      <Tours />
     </>
   );
 }
--- src/components/component11/listele.js
+++ src/components/component11/listele.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import data from "./data (3)";
 import List from "./list";
-import "./list (1).css";
+//import "./list (1).css"; component12 için yoruma aldım
 
 const Listele = () => {
   //baslangıç değeri  data isimli dizi
